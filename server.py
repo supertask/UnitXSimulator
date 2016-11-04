288bc0929e15bad5f706e9a0e5d8325b08916de6
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2016-11-03 22:54:09 -0700

    Modify an IP address.

--- server.py
+++ server.py
@@ -53,4 +53,5 @@ def do_static(filepath):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='localhost', port=port, debug=True)
+    #app.run(host='localhost', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
