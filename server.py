edbcb795f3e0f2f8b6798d1d99eb76a1a8eed0cc
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2016-11-04 16:02:09 -0700

    Modify some funcs, esp run_code() for a thread exec.

--- server.py
+++ server.py
@@ -6,72 +6,82 @@ import os
 import os.path
 from flask import Flask, request, url_for, redirect, render_template, send_from_directory
 from unitx.example import Example
+import threading
+import uuid
+import codecs
 
+lock = threading.Lock()
 app = Flask(__name__, template_folder='./static/template', static_url_path='')
-intaractive_unitx = Example(is_intaractive_run=True)
-io_unitx = Example(is_intaractive_run=False)
+#intaractive_unitx = Example(is_intaractive_run=True)
+#io_unitx = Example(is_intaractive_run=False)
+string_unitx = Example(is_intaractive_run=False)
 tmp_folder = './tmp/'
-textarea_db = tmp_folder + 'tmp_textarea.txt'
 decoding_code = 'utf-8'
 default_sample_name = 'unit_converter'
 index_template = 'unitx_template.html'
 
-def run_unitx(code):
+def __run_unitx(code):
     # TODO(Tasuku): stand in a line
-    stdout_path = tmp_folder + 'stdout_unitx.txt'
-    stderr_path = tmp_folder + 'stderr_unitx.txt'
-    if os.path.exists(stdout_path): os.remove(stdout_path)
-    if os.path.exists(stderr_path): os.remove(stderr_path)
+    u = 'x_' + str(uuid.uuid4())
+    stdout_path = tmp_folder + u + '_stdout_unitx.txt'
+    stderr_path = tmp_folder + u + '_stderr_unitx.txt'
     code = code.replace('\r', '')
+    #textarea_db = tmp_folder + u + '_textarea.txt'
 
-    import codecs
     sys.stdout = codecs.open(stdout_path,'w', decoding_code)
     sys.stderr = codecs.open(stderr_path,'w', decoding_code)
+    string_unitx.eat_string(code.encode(decoding_code))
+    """
     with open(textarea_db,'w') as wf: wf.write(code.encode(decoding_code))
     io_unitx.eat_code(textarea_db)
+    os.remove(textarea_db)
+    """
+
     sys.stdout.close()
     sys.stderr.close()
     sys.stdout = sys.__stdout__
     sys.stderr = sys.__stderr__
+
+    res = ""
     if os.path.getsize(stdout_path):
-        return open(stdout_path,"r").read().decode(decoding_code)
+        res = open(stdout_path,"r").read().decode(decoding_code)
     if os.path.getsize(stderr_path):
-        return open(stderr_path,"r").read().decode(decoding_code)
-    return ''
+        res = open(stderr_path,"r").read().decode(decoding_code)
+    os.remove(stdout_path)
+    os.remove(stderr_path)
+
+    return res
+
+
+def run_unitx(code):
+    return __run_unitx(code)
 
 @app.route("/", methods=['GET'])
 def index():
-    print "normal or GET!!"
-    if request.args.get('name'):
-        selected_name = request.args.get('name')
-        unitx_code = get_source_code(selected_name)
-        return render_template(index_template,
-            code = unitx_code,
-            selected_name = selected_name,
-            result = '')
-    else:
-        return render_template(index_template,
-            code=get_source_code(default_sample_name),
-            selected_name=default_sample_name,
-            result='')
-    """
-    elif request.args.get('about'):
-        return render_template(index_template,
-            code = 'about()',
-            selected_name = default_sample_name,
-            result = about_unitx)
-    """
-        
+    with lock:
+        if request.args.get('name'):
+            selected_name = request.args.get('name')
+            unitx_code = get_source_code(selected_name)
+            return render_template(index_template,
+                code = unitx_code,
+                selected_name = selected_name,
+                result = '')
+        else:
+            return render_template(index_template,
+                code=get_source_code(default_sample_name),
+                selected_name=default_sample_name,
+                result='')
+
 
 @app.route('/', methods=['POST','GET'])
 def run():
-    print "POST, GET!!!!!!!"
-    unitx_code = request.form['code']   
-    res = run_unitx(unitx_code)
-    return render_template(index_template,
-        code=unitx_code,
-        selected_name=request.args.get('name'),
-        result=res)
+    with lock:
+        unitx_code = request.form['code']   
+        res = run_unitx(unitx_code)
+        return render_template(index_template,
+            code=unitx_code,
+            selected_name=request.args.get('name'),
+            result=res)
 
 def get_source_code(program_name):
     try:
@@ -88,4 +98,4 @@ def do_static(filepath):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     #app.run(host='localhost', port=port, debug=True)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
